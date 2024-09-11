dab1f96951003182742bb26f1b2b14e959de74cf
Samantha <scminim100@gmail.com>
2024-09-10 22:37:34 -0700

    added placeholder footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { NavBar } from "@/components/nav";
+import { Footer } from "@/components/footer";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className="font-dinProRegular ">
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
 export const Footer = () => {
-  return <div className="bg-footerRed"></div>;
+  return (
+    <div className="bg-footerRed p-[5vw] mt-[10vh]">
+      {" "}
+      <Image
+        src="/svgs/CaseIT_Black.svg"
+        alt="CaseIt Logo Black"
+        width="100"
+        height="100"
+      />
+    </div>
+  );
 };

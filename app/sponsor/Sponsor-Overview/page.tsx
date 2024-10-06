6dcafa1afa1133049183b0eb48c81bc1b1da8c81
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2024-10-05 23:24:08 -0700

    sponsor tiles
    still need to fine tune padding for all aspect ratios, functionally complete.

--- app/sponsor/Sponsor-Overview/page.tsx
+++ app/sponsor/Sponsor-Overview/page.tsx
@@ -3,7 +3,7 @@ import { BgImgLeft } from "@/components/img";
 import * as sponsor from "@/content/sponsor_content";
 import { ImgRibbonLeft, ImgRibbonRight } from "@/components/imgRibbons";
 import { SponsorText, SponsorBanner } from "@/components/text";
-import { MobileButtons } from "@/components/sponsorBanner";
+import { MobileButtons, SponsorTiles } from "@/components/sponsorBanner";
 
 export const metadata: Metadata = {
   title: "Sponsor Overview",
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
 export default function SponsorOverview() {
   return (
     <div>
+      
       <section>
         <BgImgLeft>
           <SponsorBanner
@@ -27,10 +28,26 @@ export default function SponsorOverview() {
           </SponsorBanner>
         </BgImgLeft>
       </section>
+      <section className="flex flex-col justify-center pb-[5vh]">
+        <div className="text-header2 text-center md:pb-[4vh]">
+          {sponsor.sponsorTileContent.sectionTitle[0].title}
+        </div>
+        <div className="max-w-[80vw] mx-auto grid grid-cols-2 grid-rows-6 grid-flow-row gap-[1.644rem] md:gap-[6.5rem] md:grid-cols-3 md:grid-rows-4 xl:grid-cols-4 xl:grid-rows-3">
+          {sponsor.sponsorTileContent.sponsors.map((item, index) => (
+            <div key={index}>
+              <SponsorTiles 
+                img={sponsor.sponsorTileContent.sponsors[index].img} 
+                text={sponsor.sponsorTileContent.sponsors[index].text} 
+                alt={sponsor.sponsorTileContent.sponsors[index].alt}  
+              />
+            </div>
+          ))}
+        </div>
+      </section>
       <section>
-        <div className="text-header2 text-center md:pb-[8vh]">
+        <div className="text-header2 text-center pb-[5rem] md:pb-[7.5rem]">
           {sponsor.sponsorOverviewText.sectionTitle[0].title}
-        </div>{" "}
+        </div>
         <div className="md:flex md:flex-col md:space-x-[2vw] md:space-y-[20vh]">
           {sponsor.sponsorOverviewText.ribbons.map((item, index) => (
             <div key={index}>

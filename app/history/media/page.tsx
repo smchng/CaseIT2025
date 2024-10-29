9f3c64f958f501b4761b8a2571860605a73f0d67
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2024-10-29 12:13:44 -0700

    fixed banner

--- app/history/media/page.tsx
+++ app/history/media/page.tsx
@@ -16,29 +16,34 @@ export const metadata: Metadata = {
 export default function Media() {
   return (
     <div>
-      <section>
-        <BgImgLeft>
-        <SponsorBanner
-            title={media.mediaText.header1}
-            text={media.mediaText.paragraph}
-          />
-          {/* { <div className="text-header1">{media.mediaText.header1}</div>{" "}
-          <p className="pb-[4vh]">
-            <div className="text-paragraph">{media.mediaText.paragraph} </div>
-          </p> */}
-          <div className="flex space-x-[3vw]"> 
+      <section className="relative mb-[10vh]  h-[90vh] w-full">
+
+        <div className="absolute inset-0 flex flex-col justify-center">
+        <div className="w-full flex items-center justify-center md:justify-start">
+          <div className="flex flex-col space-y-[2vh] max-w-[85vw] md:max-w-[50vw] xl:max-w-[40vw] mx-auto md:mx-[5vw] xl:mx-[10vw] text-left">
+            <div className="text-header2">{media.mediaText.header1}</div>
+            <div className="text-paragraph">
+              <p className="text-paragraph">{media.mediaText.paragraph}</p>
+            </div>
+            </div>
+        </div>
+          <div className="flex flex-col md:flex-row pt-4 pl-[8vw] max-w-[85vw] md:pl-0 md:max-w-[50vw] xl:max-w-[40vw] md:mx-[5vw] xl:mx-[10vw] text-left"> 
             <RoundedButton
               text={media.buttonText[0].text}
               link={media.buttonText[0].link}
               variant="red"
             />
-            <RoundedButton
-              text={media.buttonText[1].text}
-              link={media.buttonText[1].link}
-              variant="black"
-            />
+            <div className="pt-2 md:pl-2 md:pt-0">
+              <RoundedButton
+                text={media.buttonText[1].text}
+                link={media.buttonText[1].link}
+                variant="black"
+              />
+            </div>
+            
           </div>
-        </BgImgLeft>
+        </div>
+
       </section>
       <section>
       <div className="grid mx-auto max-w-[80%] md:grid-cols-2 md:grid-rows-6 gap-[1.644rem]">

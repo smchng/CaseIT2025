461a60240e7ed95167558e6822f89f170437453f
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2024-10-30 16:38:27 -0700

    made card a clickable

--- components/mediaItems.tsx
+++ components/mediaItems.tsx
@@ -15,25 +15,25 @@ type galleryItems = {
 };
 
 
-export const Gallery = ({ title, subtext, img, alt }: galleryItems) => {
+export const Gallery = ({ title, subtext, img, alt, link }: galleryItems) => {
   return (
-    <div className="flex flex-col md:max-w-[100%]">
-      <div className="rounded-[2rem] border-[#E7E7E7] border-2 py-2">
+    <Link href={link || "dummy"} className="flex flex-col">
+      <div className="rounded-[2rem] border-[#E7E7E7] border-2 p-5">
         <div>
           <Image
             src={img || "/imgs/mediaGallery.png"}
             alt={alt || "Circle of people doing a cheer"}
             width={472}
             height={267}
-            className="mx-auto size-[95%]"
+            className="mx-auto w-full"
           />
         </div>
         <div className="text-center pt-2">
           <div className="text-paragraph">{title}</div>
-          <div className="text-paragraph text-greyLight pb-2">{subtext}</div>
+          <div className="text-paragraph text-greyLight ">{subtext}</div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 

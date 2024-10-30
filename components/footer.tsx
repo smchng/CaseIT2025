168e826fc596e78e3af5c1af3b83970e51bdabf9
Michael Dresler <michaeldresler@hotmail.com>
2024-10-30 10:28:48 -0700

    Revert "footer fixes"
    This reverts commit ed4f526b964c3dda6e4dc7b5fd66f3469435474d.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,20 +1,19 @@
 import { NavList } from "./NavList";
 import CaseitLogoWhite from "@/public/svgs/CaseitLogoWhite";
 import {allLinks, socialLinks} from "@/content/footer_content"
-import Link from "next/link";
 
 
 export const Footer = () => {
 
 
   return (
-    <div className="bg-footerRed px-6 md:px-12 xl:px-20 py-14 lg:pt-24 mt-[10vh] flex flex-col gap-8 lg:gap-[10vw] 2xl:gap-[20vw] lg:flex-row text-white text-[0.875rem] lg:text-[1rem] ">
+    <div className="bg-footerRed px-9 md:px-12 xl:px-20 py-14 lg:pt-24 mt-[10vh] flex flex-col gap-8 lg:gap-36 md:flex-row text-white text-[0.5rem] lg:text-[1.125rem] ">
       <div className=" max-w-[87px] lg:max-w-[165px]">
         <CaseitLogoWhite className="w-full" />
       </div>
       <nav className="flex flex-col w-full">
-        <div className="flex flex-col  gap-12  ">
-          <div className="grid grid-cols-2 gap-y-12 md:gap-8 justify-between sm:flex  ">
+        <div className="flex flex-col md:flex-row lg:flex-col gap-12 ">
+          <div className="grid grid-cols-2 gap-y-12 sm:flex max-w-[210px] sm:max-w-none  justify-between w-full ">
             {allLinks.map((link, index) => (
               <div key={index}>
                 <p className="font-bold mb-6">{link.title}</p>
@@ -24,16 +23,16 @@ export const Footer = () => {
           </div>
           <div>
             <p className="font-bold mb-6">Follow us!</p>
-            <NavList listClassName="flex flex-row gap-2" linkClassName="flex opacity-60 hover:opacity-100 duration-300 " linkArray={socialLinks} />
+            <NavList listClassName="flex flex-col gap-2" linkClassName="flex opacity-60 " linkArray={socialLinks} />
           </div>
         </div>
 
-        <div className="flex flex-col sm:flex-row gap-4 text-[0.75rem] md:text-[1rem]  mt-24 border-t pt-4 border-t-white text-white/60">
-          <span>CaseIT © 2020-2025 </span>
-          <span className="hidden sm:flex">| </span>
-          <Link className="hover:text-white duration-200" href={"/privacypolicy"}>Privacy Policy </Link>
-          <span className="hidden sm:flex">| </span>
-          <Link className="hover:text-white duration-200" href={"/tradterritories"}>Traditional Territories Acknowledgement</Link>
+        <div className="flex flex-col sm:flex-row gap-4  mt-24 border-t pt-4 border-t-white text-white/60">
+          <p>CaseIT © 2020-2025 </p>
+          <p className="hidden sm:flex">| </p>
+          <p>Privacy Policy </p>
+          <p className="hidden sm:flex">| </p>
+          <p>Traditional Territories Acknowledgement</p>
         </div>
       </nav>
     </div>

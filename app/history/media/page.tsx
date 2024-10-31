62dccc2854d858dbd0c08f655cb1a08f69624242
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2024-10-30 22:06:58 -0700

    back to top button + banner formatting

--- app/history/media/page.tsx
+++ app/history/media/page.tsx
@@ -16,18 +16,18 @@ export const metadata: Metadata = {
 export default function Media() {
   return (
     <div>
-      <section className="relative h-[90vh] mb-[-20vh] w-full">
+      <section id="MediaTop" className="relative h-[90vh] mb-[-20vh] w-full">
 
         <div className="absolute inset-0 flex flex-col justify-center">
         <div className="w-full flex items-center justify-center md:justify-start">
-          <div className="flex flex-col space-y-[2vh] max-w-[85vw] md:max-w-[50vw] xl:max-w-[40vw] mx-auto md:mx-[10vw] xl:mx-[10vw] text-left">
+          <div className="flex flex-col space-y-[2vh] max-w-[85vw] md:max-w-[50vw] xl:max-w-[40vw] mx-[8vw] md:mx-[15vw] xl:mx-[15vw] text-left">
             <div className="text-header2">{media.mediaText.header1}</div>
             <div className="text-paragraph">
               <p className="text-paragraph">{media.mediaText.paragraph}</p>
             </div>
             </div>
         </div>
-          <div className="flex flex-col md:flex-row pt-4 pl-[8vw] max-w-[85vw] md:pl-0 md:max-w-[50vw] xl:max-w-[40vw] md:mx-[10vw] xl:mx-[10vw] text-left"> 
+          <div className="flex flex-col md:flex-row pt-4 pl-[8vw] max-w-[85vw] md:pl-0 md:max-w-[50vw] xl:max-w-[40vw] md:mx-[15vw] xl:mx-[15vw] text-left"> 
             <RoundedButton
               text={media.buttonText[0].text}
               link={media.buttonText[0].link}
@@ -58,6 +58,9 @@ export default function Media() {
           ))}
         </div>
         
+      </section>
+      <section className="md:hidden flex justify-center mx-auto scroll-smooth pt-[7vh]">
+        <RoundedButton text="Back to top" link="#MediaTop" variant="red"/> 
       </section>
     </div>
   );

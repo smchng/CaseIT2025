277a32eefb7aa23a0c38b455a4efaaf1130a258b
Michael Dresler <michaeldresler@hotmail.com>
2024-10-31 01:37:44 -0700

    added content directory to tailwind config

--- components/NavList.tsx
+++ components/NavList.tsx
@@ -16,7 +16,6 @@ type NavListProps = {
   activePathname?: string;
 };
 
-
 export const NavList = ({
   linkArray,
   listClassName,
@@ -31,27 +30,40 @@ export const NavList = ({
         <li
           key={index}
           className={twMerge(
-            `${linkClassName} relative ${
-              activePathname === item.ref && "text-black"
+            `${linkClassName} relative ${activePathname === item.ref && "text-black"
             }`,
-            item?.className
+            item.className
           )}
         >
-          <div onMouseEnter={() => setActive(item.ref)} onMouseLeave={() => setActive(null)}>
-          {item.sublinks?(<div className="cursor-default">{item.name}</div>): <Link href={item.ref}>{item.name}</Link>}
+          <div
+            onMouseEnter={() => setActive(item.ref)}
+            onMouseLeave={() => setActive(null)}
+          >
+            {item.sublinks ? (
+              <div className="cursor-default">{item.name}</div>
+            ) : (
+              <Link href={item.ref}>{item.name}</Link>
+            )}
 
             {/* Dropdown for sublinks with opacity transition */}
             {item.sublinks && (
               <div
                 className={`hidden md:block absolute left-1/2 -translate-x-1/2 transition-opacity duration-300 pt-4  ${
-                  isActive === item.ref ? "opacity-100" : "opacity-0 pointer-events-none"
+                  isActive === item.ref
+                    ? "opacity-100"
+                    : "opacity-0 pointer-events-none"
                 }`}
               >
-                <div className=" absolute w-16 h-16 bg-white drop-shadow- left-1/2 -translate-x-1/2 rounded-lg rotate-45 "> </div>
+                <div className=" absolute w-16 h-16 bg-white drop-shadow- left-1/2 -translate-x-1/2 rounded-lg rotate-45 ">
+                  {" "}
+                </div>
                 <ul className="py-8 px-6 z-[1999] flex flex-col gap-4 w-64 rounded-xl bg-white drop-shadow-sm">
                   {item.sublinks.map((sublink, index) => (
                     <li key={index}>
-                      <Link href={sublink.ref} className="text-greyLight hover:text-black duration-300">
+                      <Link
+                        href={sublink.ref}
+                        className="text-greyLight hover:text-black duration-300"
+                      >
                         {sublink.name}
                       </Link>
                     </li>

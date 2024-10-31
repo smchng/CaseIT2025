89656780306657ee74fae9f93ece268ded954117
Samantha <scminim100@gmail.com>
2024-10-31 10:33:00 -0700

    media banner padding

--- app/history/media/page.tsx
+++ app/history/media/page.tsx
@@ -13,29 +13,25 @@ export const metadata: Metadata = {
 export default function Media() {
   return (
     <div>
-      <section id="MediaTop" className="relative h-[90vh] mb-[-15vh] w-full">
-        <div className="absolute inset-0 flex flex-col justify-center">
-          <div className="w-full flex items-center justify-center md:justify-start">
-            <div className="flex flex-col space-y-[2vh] max-w-[85vw] md:max-w-[50vw] xl:max-w-[40vw] mx-[8vw] md:mx-[15vw] xl:mx-[15vw] text-left">
-              <div className="text-header2">{media.mediaText.header1}</div>
-              <div className="text-paragraph">
-                <p className="text-paragraph">{media.mediaText.paragraph}</p>
-              </div>
-            </div>
+      <section id="MediaTop" className="relative w-full py-[15vh]">
+        <div className="w-full flex items-center justify-center md:justify-start pb-[3vh] md:pb-[2vh]">
+          <div className="flex flex-col space-y-[2vh] max-w-[85vw] md:max-w-[50vw] xl:max-w-[40vw] mx-[8vw] md:mx-[15vw] xl:mx-[15vw] text-left">
+            <div className="text-header2">{media.mediaText.header1}</div>
+            <p className="text-paragraph">{media.mediaText.paragraph}</p>
           </div>
-          <div className="flex flex-col md:flex-row pt-4 pl-[8vw] max-w-[85vw] md:pl-0 md:max-w-[50vw] xl:max-w-[40vw] md:mx-[15vw] xl:mx-[15vw] text-left">
+        </div>
+        <div className="flex flex-col md:flex-row pt-4 pl-[8vw] max-w-[85vw] md:pl-0 md:max-w-[50vw] xl:max-w-[40vw] md:mx-[15vw] xl:mx-[15vw] text-left">
+          <RoundedButton
+            text={media.buttonText[0].text}
+            link={media.buttonText[0].link}
+            variant="red"
+          />
+          <div className="pt-2 md:pl-2 md:pt-0">
             <RoundedButton
-              text={media.buttonText[0].text}
-              link={media.buttonText[0].link}
-              variant="red"
+              text={media.buttonText[1].text}
+              link={media.buttonText[1].link}
+              variant="black"
             />
-            <div className="pt-2 md:pl-2 md:pt-0">
-              <RoundedButton
-                text={media.buttonText[1].text}
-                link={media.buttonText[1].link}
-                variant="black"
-              />
-            </div>
           </div>
         </div>
       </section>

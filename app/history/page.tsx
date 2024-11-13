e3a3cba4a5cfac82b096f2051513b97df227a693
Samantha <scminim100@gmail.com>
2024-11-13 01:51:15 -0800

    mobile cheanges

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import * as text from "@/components/text";
 import { ImgButton } from "@/components/buttons";
 import * as history from "@/content/history_content";
-import { CompetitionCard } from "@/components/HistoryCard"
-import { competitionData } from '@/content/history_content';
-import mypic from "@/public/imgs/2024_OC.png"
+import { CompetitionCard } from "@/components/HistoryCard";
+import { competitionData } from "@/content/history_content";
+import mypic from "@/public/imgs/2024_OC.png";
 
 export const metadata: Metadata = {
   title: "History",
@@ -15,31 +15,32 @@ export const metadata: Metadata = {
 export default function History() {
   return (
     <div>
-      <section className=" flex flex-col bg-[#f3f3f3] overflow-hidden">
+      <section className=" flex flex-col pb-10 overflow-hidden">
         <div className="px-4 md:pl-32 pt-24">
-           <h2 className="text-xl md:text-4xl font-semibold">{history.heroContent.header}</h2>
-      <h3 className="font-medium">{history.heroContent.subtitle}</h3>
-      <p className="mt-6 max-w-[65ch]">{history.heroContent.paragraph} </p>
-
+          <h2 className="text-header2  font-semibold">
+            {history.heroContent.header}
+          </h2>
+          <h3 className="font-medium">{history.heroContent.subtitle}</h3>
+          <p className="mt-6 max-w-[65ch]">{history.heroContent.paragraph} </p>
         </div>
-     
-      <div className="mt-8 flex gap-4 pl-4 flex-nowrap w-fit  py-6 animate-marquee hover:pause animation-delay ">
-      {competitionData.map((competition, index) => (
-        <CompetitionCard
-          key={index}
-          image={competition.img}
-          title={competition.title}
-          description={competition.description}
-          winner={competition.winner}
-        />
-      ))}
-    </div>
 
+        <div className="mt-8 flex flex-col px-[10vw] space-y-8 md:space-y-0 md:px-0 md:flex-row md:gap-4 md:pl-4 md:flex-nowrap md:w-fit  md:py-6 md:animate-marquee md:hover:pause md:animation-delay ">
+          {competitionData.map((competition, index) => (
+            <CompetitionCard
+              key={index}
+              image={competition.img}
+              title={competition.title}
+              description={competition.description}
+              winner={competition.winner}
+            />
+          ))}
+        </div>
       </section>
 
-      
       <section className="w-full flex flex-col items-center py-[8vh]">
-        <div className="text-header2">{history.historyText.header2}</div>
+        <div className="text-header4 pb-3 md:text-header2">
+          {history.historyText.header2}
+        </div>
         <div className="flex flex-col space-y-[2vw] md:space-y-[0] md:flex-row md:space-x-[2vw] md:pt-[2vh]">
           {history.imgButtons.map((item, index) => (
             <ImgButton

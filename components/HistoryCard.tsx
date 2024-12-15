882989579109301b956d22edf40832241fa5f179
Michael Dresler <michaeldresler@hotmail.com>
2024-12-15 00:09:01 -0800

    update history card "winner"
    applied margin-top auto so it now always aligns to bottom of the card

--- components/HistoryCard.tsx
+++ components/HistoryCard.tsx
@@ -15,7 +15,7 @@ export const CompetitionCard = ({ title, description, winner, className,image }:
         <Image className="rounded-2xl mb-4 aspect-[23/15] object-cover" src={image} alt={""} ></Image>
       <h2 className="text-xl font-bold mb-2">{title}</h2>
       <p className="text-gray-700 mb-4">{description}</p>
-      <div className="text-gray-500">
+      <div className="text-gray-500 mt-auto">
         <strong>Winner:</strong> {winner}
       </div>
     </div>

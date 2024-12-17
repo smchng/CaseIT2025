d9750e95cbbedeec6eba34f81baa402a84df576e
Michael Dresler <michaeldresler@hotmail.com>
2024-12-16 18:09:58 -0800

    getinvolved page finished
    -moved carousel into its own component
    -added framer-motion for slider functionality

--- app/2025/Get-Involved/page.tsx
+++ app/2025/Get-Involved/page.tsx
@@ -3,8 +3,8 @@ import { RoundedButton, ArrowButton } from "@/components/buttons";
 import { BgImgCenter } from "@/components/img";
 import banner from "@/public/imgs/banners/2025_banner.png";
 import * as involved from "@/content/involved_content";
-import { GetInvolvedCard } from "@/components/GetInvolvedCard";
-import { memberData } from "@/content/GetInvolved_content";
+import Carousel from "@/components/Carousel";
+
 
 export const metadata: Metadata = {
   title: "Get Involved",
@@ -16,14 +16,14 @@ export default function GetInvolved() {
     <div>
       <section>
         <BgImgCenter img={banner}>
-          <div>
-            <div className="text-redDark text-header4">
+          <div className="flex flex-col items-center">
+            <div className="text-redDark text-center text-header4">
               {involved.involvedText.red}
             </div>
-            <div className="text-header1 pb-8 max-w-[50vw]">
+            <div className="text-header1 text-center pb-8 max-w-[50vw]">
               {involved.involvedText.header1}
             </div>
-            <div className="flex flex-row items-center justify-center gap-2">
+            <div className="flex flex-col md:flex-row items-center justify-center gap-4">
               <RoundedButton
               text="Apply now"
               link="/history"
@@ -40,22 +40,12 @@ export default function GetInvolved() {
         </BgImgCenter>
       </section>
       <section className="  mt-24 ">
-        <h2 className="w-full text-header2 ml-8 lg:ml-32 mb-12 max-w-[24ch] tracking-tight leading-tight">How has CaseIT impacted students’ university journey?</h2>
+        <h2 className=" text-header2 ml-8 lg:ml-32 mb-12 max-w-[24ch] tracking-tight leading-tight">How has CaseIT impacted students’ university journey?</h2>
+        <Carousel/>
 
-        <div className="relative ">
-        <div className="w-[200px] h-full hidden lg:block absolute top-0 left-0 bg-white/100  [mask-image:linear-gradient(90deg,_rgba(0,0,0,1)_10%,_rgba(255,255,255,0)_60%)]  backdrop-blur-[10px]" ></div>
-        {/* <div className="w-[300px] h-full absolute top-0 right-0 rotate-180 bg-white/100  [mask-image:linear-gradient(90deg,_rgba(0,0,0,1)_15%,_rgba(255,255,255,0)_80%)]  backdrop-blur-[4px]" ></div> */}
 
-        <div className="pl-8  lg:pl-32 flex flex-row gap-8 overflow-scroll p-3">
-        {memberData.map((member, index) => (
-          <>
-          <GetInvolvedCard key={index} image={member.img} name={member.name} quote={member.quote} position={member.position}/>
-          </>
-        ))}
-        </div>
-        </div>
       </section>
-      <section className="flex flex-col justify-center items-center gap-6 py-28">
+      <section className="flex flex-col justify-center items-center gap-6 py-28 px-4">
         <h2 className="text-header2 text-center">Open Positions</h2>
         <p className="text-lg text-center max-w-[55ch] ">There are no open positions for now! Follow @caseitsfu on Instagram to get the latest updates on our hiring initiatives!</p>
         <RoundedButton

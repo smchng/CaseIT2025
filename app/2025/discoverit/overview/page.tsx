97e8f1d6a78b5b492eb0d17fc098eb21437a101b
Samantha <scminim100@gmail.com>
2025-01-16 18:38:04 -0800

    overview donezo

--- app/2025/discoverit/overview/page.tsx
+++ app/2025/discoverit/overview/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from "next";
 import { DiscoverITBanner } from "@/components/subpageBanner";
-import { Bento } from "@/components/discoverITItems";
+import { Bento, BentoText } from "@/components/discoverITItems";
 import { overviewText } from "@/content/discoverIT_content";
+import { RoundedButton } from "@/components/buttons";
 
 export const metadata: Metadata = {
   title: "DiscoverIT - Overview",
@@ -14,12 +15,44 @@ export default function Overview() {
       <div className="pb-8">
         <DiscoverITBanner></DiscoverITBanner>
       </div>
-      <section className="pb-5">
-        <Bento title="About" text={overviewText.paragraph[0]}></Bento>
+      <section className="pb-6">
+        <Bento>
+          {" "}
+          <div className="md:grid md:grid-cols-2 md:gap-6">
+            <BentoText
+              title={overviewText.title[0]}
+              text={overviewText.paragraph[0]}
+            ></BentoText>{" "}
+            <BentoText
+              title={overviewText.title[1]}
+              text={overviewText.paragraph[1]}
+            ></BentoText>
+          </div>{" "}
+        </Bento>
       </section>
-      <section className="md:flex md:space-x-5">
-        <Bento title="About" text={overviewText.paragraph[0]}></Bento>
-        <Bento title="About" text={overviewText.paragraph[0]}></Bento>
+      <section className="md:grid md:grid-cols-2 md:gap-6">
+        <Bento>
+          <BentoText
+            title={overviewText.title[1]}
+            text={overviewText.paragraph[1]}
+          ></BentoText>
+          <div className="mt-auto">
+            <RoundedButton
+              text="Explore"
+              link="/2025/DiscoverIT/CaptureIT"
+              variant="red"
+            />
+          </div>
+        </Bento>
+
+        <Bento>
+          <BentoText
+            title={overviewText.title[2]}
+            text={overviewText.paragraph[2]}
+          ></BentoText>
+          <BentoText text={overviewText.paragraph[3]}></BentoText>
+          <BentoText text={overviewText.paragraph[4]}></BentoText>
+        </Bento>
       </section>
     </div>
   );
--- components/discoverITItems.tsx
+++ components/discoverITItems.tsx
@@ -1,13 +1,23 @@
+"use client";
 import { BoldedText } from "./text";
 type discoverITItems = {
-  title: string;
-  text: string;
+  title?: string;
+  text?: string;
+  children?: React.ReactNode;
 };
 
-export const Bento = ({ title, text }: discoverITItems) => {
+export const Bento = ({ children }: discoverITItems) => {
   return (
-    <div className=" p-7 bg-greyDark rounded-2xl drop-shadow-[0_0_5px_rgba(0,0,0,0.2)]">
-      <h1 className="text-header2">{title}</h1>
+    <div className="flex flex-col p-7 bg-greyDark rounded-2xl drop-shadow-[0_0_5px_rgba(0,0,0,0.2)]">
+      {children}
+    </div>
+  );
+};
+
+export const BentoText = ({ title, text }: discoverITItems) => {
+  return (
+    <div>
+      <h1 className="text-header2 pb-4">{title}</h1>
       <BoldedText text={text} />
     </div>
   );

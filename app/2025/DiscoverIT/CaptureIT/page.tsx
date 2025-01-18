ebd79273867738d9b455a5eba76664b8c6901196
Samantha <scminim100@gmail.com>
2025-01-17 20:42:40 -0800

    added saptureit page coe

--- app/2025/DiscoverIT/CaptureIT/page.tsx
+++ app/2025/DiscoverIT/CaptureIT/page.tsx
@@ -1,10 +1,43 @@
 import { Metadata } from "next";
-
+import { DiscoverITBanner } from "@/components/subpageBanner";
+import {
+  CaptureITBanner,
+  CaptureITCard,
+  CaptureITPosts,
+} from "@/components/discoverITItems";
+import { CaptureITCardText, CaptureITImgs } from "@/content/discoverIT_content";
 export const metadata: Metadata = {
   title: "DiscoverIT - CaptureIT",
   description: "Learn more about our company and team.",
 };
 
 export default function CaptureIT() {
-  return <div></div>;
+  return (
+    <div className="py-[20vh] px-7 md:px-16 xl:px-[15vw]">
+      <div className="pb-8">
+        <DiscoverITBanner></DiscoverITBanner>
+      </div>
+      <section className="md:flex md:flex-col lg:grid lg:grid-cols-2">
+        <div className="pb-[10vh]">
+          <CaptureITBanner></CaptureITBanner>
+          {CaptureITCardText.content.map((item, index) => (
+            <div key={index}>
+              <CaptureITCard
+                title={CaptureITCardText.content[index].header}
+                text={CaptureITCardText.content[index].paragraph}
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="pb-6">
+          {CaptureITImgs.imgs.map((item, index) => (
+            <div key={index}>
+              <CaptureITPosts img={CaptureITImgs.imgs[index]} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }

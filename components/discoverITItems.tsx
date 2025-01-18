ff50529e5a3bd11658ab62cc481a971fd8c86ae4
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2025-01-17 19:31:20 -0800

    final page

--- components/discoverITItems.tsx
+++ components/discoverITItems.tsx
@@ -51,3 +51,48 @@ export const TourBento = ({
     </div>
   );
 };
+
+export const CaptureITBanner = ({
+}: discoverITItems) => {
+  return (
+    <div>
+      <div className="text-header2">
+        <p>Capture<span className="text-red">IT</span></p>   
+      </div>
+      <div className="pt-[3%]">
+        <p>Capture memorable moments of your team exploring Vancouver and upload them on Instagram to earn points and win the SpirIT prize! Make sure to set your settings to public and mention <span className="text-red">@caseitsfu</span>  along with a hashtag of your university (e.g. #simonfraseruniversity).</p>
+      </div>
+    </div>
+  );
+};
+
+export const CaptureITCard = ({
+  title,
+  text,
+}: discoverITItems) => {
+  return (
+    <div className={"flex flex-col p-4 rounded-2xl shadow-[0px_0px_9px_1.5px_rgba(0,0,0,0.3)] bg-white md:min-w-[350px] mt-6 min-h-[16vh]"}>
+       <div className="text-red text-paragraph font-bold">{title}</div>
+       <p className="pt-1 text-paragraph">{text}</p>
+    </div>
+  );
+};
+
+export const CaptureITPosts = ({
+  img,
+}: discoverITItems) => {
+  return (
+    <div className={"flex justify-center lg:pb-[3vh] pb-[7vh]"}>
+      <Image
+        src={img || "/imgs/story1"}
+        alt="caseitsfu's instagram story"
+        width={389} // Original aspect ratio width
+        height={690.916} // Original aspect ratio height
+        // className="rounded-xl  max-w-[45vw] lg:max-w-[25vw]"
+      />
+    </div>
+  );
+};
+
+
+  

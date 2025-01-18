c1543b055b3825c76faf5bc76b25d85a4e212a89
Samantha <scminim100@gmail.com>
2025-01-17 21:18:00 -0800

    tour image sizing

--- components/discoverITItems.tsx
+++ components/discoverITItems.tsx
@@ -41,46 +41,54 @@ export const TourBento = ({
         <div className="text-paragraph">{text}</div>
         <div className="text-paragraph">{children}</div>
       </div>
-      <Image
-        src={img || "/imgs/Downtown-Vancouver.png"}
-        alt="its an image"
-        width={606} // Original aspect ratio width
-        height={908} // Original aspect ratio height
-        className="rounded-xl  max-w-[45vw] lg:max-w-[25vw]"
-      />
+      <div className="justify-center w-full flex">
+        <Image
+          src={img || "/imgs/Downtown-Vancouver.png"}
+          alt="its an image"
+          width={606} // Original aspect ratio width
+          height={908} // Original aspect ratio height
+          className="rounded-xl w-full lg:w-auto md:max-w-[60vw] lg:max-w-[25vw]"
+        />
+      </div>
     </div>
   );
 };
 
-export const CaptureITBanner = ({
-}: discoverITItems) => {
+export const CaptureITBanner = ({}: discoverITItems) => {
   return (
     <div>
       <div className="text-header2">
-        <p>Capture<span className="text-red">IT</span></p>   
+        <p>
+          Capture<span className="text-red">IT</span>
+        </p>
       </div>
       <div className="pt-[3%]">
-        <p>Capture memorable moments of your team exploring Vancouver and upload them on Instagram to earn points and win the SpirIT prize! Make sure to set your settings to public and mention <span className="text-red">@caseitsfu</span>  along with a hashtag of your university (e.g. #simonfraseruniversity).</p>
+        <p>
+          Capture memorable moments of your team exploring Vancouver and upload
+          them on Instagram to earn points and win the SpirIT prize! Make sure
+          to set your settings to public and mention{" "}
+          <span className="text-red">@caseitsfu</span> along with a hashtag of
+          your university (e.g. #simonfraseruniversity).
+        </p>
       </div>
     </div>
   );
 };
 
-export const CaptureITCard = ({
-  title,
-  text,
-}: discoverITItems) => {
+export const CaptureITCard = ({ title, text }: discoverITItems) => {
   return (
-    <div className={"flex flex-col p-4 rounded-2xl shadow-[0px_0px_9px_1.5px_rgba(0,0,0,0.3)] bg-white md:min-w-[350px] mt-6 min-h-[16vh]"}>
-       <div className="text-red text-paragraph font-bold">{title}</div>
-       <p className="pt-1 text-paragraph">{text}</p>
+    <div
+      className={
+        "flex flex-col p-4 rounded-2xl shadow-[0px_0px_9px_1.5px_rgba(0,0,0,0.3)] bg-white md:min-w-[350px] mt-6 min-h-[16vh]"
+      }
+    >
+      <div className="text-red text-paragraph font-bold">{title}</div>
+      <p className="pt-1 text-paragraph">{text}</p>
     </div>
   );
 };
 
-export const CaptureITPosts = ({
-  img,
-}: discoverITItems) => {
+export const CaptureITPosts = ({ img }: discoverITItems) => {
   return (
     <div className={"flex justify-center lg:pb-[3vh] pb-[7vh]"}>
       <Image
@@ -93,6 +101,3 @@ export const CaptureITPosts = ({
     </div>
   );
 };
-
-
-  

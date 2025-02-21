14ade9d98a945a030208fc20c2d7e5d7f5cc6e33
Samantha <scminim100@gmail.com>
2025-02-21 13:15:44 -0800

    revised code

--- components/2025components/results.tsx
+++ components/2025components/results.tsx
@@ -1,21 +1,20 @@
 import Image from "next/image";
 
 type resultsProps = {
-  title: string;
+  title: string | JSX.Element;
   img: string;
   alt: string;
 };
 
 export const Results = ({ title, img, alt }: resultsProps) => {
   return (
-    <div>
-      <div className="text-header2">{title}</div>
+    <div className="mt-[20vh] flex flex-col gap-4 p-7 md:p-0 md:max-w-[80vw] mx-auto">
+      {title}
       <Image
-        src={img || "/imgs/2024_OC.png"}
-        alt={alt || "case results"}
-        width={2080}
-        height={2080}
-        className="w-[60vw]"
+        src={img || "/imgs/compWeek/C1_win.png"}
+        alt={alt || "case 1 winner"}
+        width={1100}
+        height={436}
       />
     </div>
   );
